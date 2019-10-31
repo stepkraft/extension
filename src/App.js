5cc970dc0c76b5a933fe3a2c667ef3b71fff096c
Stsiapan Krautsevich <stepkraft@gmail.com>
2019-11-01 02:08:15 +0300

    initial tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
+const buttons = [
+  { id: 'rating', label: 'Rating', disabled: true },
+  { id: 'address', label: 'www address' },
+  { id: 'bonuses', label: 'Bonuses' },
+  { id: 'feedbacks', label: 'Feedbacks', disabled: true },
+  { id: 'prices', label: 'Prices', disabled: true },
+];
+
 function App() {
+  const [key, setKey] = useState('address');
+  const [extended, setExtended] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tab.Container id="tabs-buttons" defaultActiveKey={key}>
+      <Row>
+        <Col xs='auto' sm={12} md={8} lg={7}>
+          <Nav variant={!!extended ? 'tabs' : 'pills'} active={key}>
+            {buttons.map(({ id, label, disabled }) =>
+              <Nav.Item>
+                <Nav.Link
+                  active={key === id}
+                  eventKey={id}
+                  disabled={disabled}
+                  onSelect={k => {
+                    setKey(k);
+                    setExtended(true);
+                  }}
+                >{label}</Nav.Link>
+              </Nav.Item>
+            )}
+          </Nav>
+          {!!extended &&
+            <Tab.Content>
+              {buttons.filter(({disabled}) => !disabled ).map(({ id }) =>
+                <Tab.Pane eventKey={id}>
+                  <p>{`content for ${id} will be soon`}</p>
+                </Tab.Pane>
+              )}
+            </Tab.Content>
+          }
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
 
