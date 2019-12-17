baff818115e4b1dfd9e2117dc98aea1fa982259f
Stsiapan Krautsevich <stepkraft@gmail.com>
2019-12-18 00:19:25 +0300

    bonuses screen

--- src/components/TabsContents/BonusesContent.js
+++ src/components/TabsContents/BonusesContent.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import { Container, Header, List, Image } from 'semantic-ui-react'
+import React, { useContext } from 'react';
+import { Container, Image, Grid, Segment, Button, Divider } from 'semantic-ui-react'
+import { get } from 'lodash'
+import AppContext from '../../services/AppContext';
+import aliLogo from '../../assets/1280px-Aliexpress_logo1.png';
+import puppySportLogo from '../../assets/puppy-sport-logo.png';
+import trustsellersLogo from '../../assets/trustsellers-logo.png';
+import bonusCoupon from '../../assets/bonus-coupon.png';
 
-const otherBonuses = [
-    { logo: 'https://www.shareicon.net/data/512x512/2017/06/08/886802_logo_512x512.png', text: '5% cashback', url: 'https://taobao.com/' },
+const bonuses = [
+  { logo: aliLogo, url: 'aliexpress.com', coupon: bonusCoupon, active: true, main: true, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: puppySportLogo, url: 'www.puppyspot.com', coupon: bonusCoupon, active: true, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: trustsellersLogo, url: 'trustsellers.com', coupon: bonusCoupon, active: false, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: puppySportLogo, url: 'www.puppyspot.com', coupon: bonusCoupon, active: true, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: aliLogo, url: 'aliexpress.com', coupon: bonusCoupon, active: false, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: aliLogo, url: 'www.puppyspot.com', coupon: bonusCoupon, active: false, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
+  { logo: trustsellersLogo, url: 'trustsellers.com', coupon: bonusCoupon, active: true, main: false, bonusText: 'Скидка на первую покупку после регистрации' },
 ];
 
 const BonusesContent = () => {
+  const { currentLangData } = useContext(AppContext);
+
     return (
-      <Container>
-        <List divided relaxed>
-          <List.Item>
-            <Image avatar src='https://cdn.iconscout.com/icon/free/png-512/aliexpress-569572.png' />
-            <List.Content>
-              <List.Header as='a'>Bonus: 5$</List.Header>
-              <a href='https://www.aliexpress.com/'>https://www.aliexpress.com/</a>
-            </List.Content>
-          </List.Item>  
-        </List>
-    
-        <Header as='h4'>Other bonuses</Header>
-        <List divided relaxed>
-          {otherBonuses.map(({logo, text, url}, idx) =>
-            <List.Item key={idx}>
-              <Image avatar src={logo} />
-              <List.Content>
-                <List.Header as='a'>{text}</List.Header>
-                <a href={url}>{url}</a>
-              </List.Content>
-            </List.Item> 
-          )}
-        </List>
+      <Container className='ext-bonuses-container'>
+        <Grid stackable columns={2}>
+          {bonuses.map((bonus, idx) => (
+            <Grid.Column key={idx}>
+              <Segment secondary={!!bonus.main}>
+                <Container textAlign='center'>
+                  <Image src={bonus.logo} fluid />
+                  <a href={bonus.url}>{bonus.url}</a>
+                </Container>
+                <Divider hidden />
+                <Container textAlign='center'>
+                  <Image src={bonus.coupon} fluid />
+                </Container>
+                <Divider hidden />
+                <Container textAlign='center'>
+                  {bonus.bonusText}
+                </Container>
+                <Divider hidden />
+                <Container textAlign='center'>
+                  <Button color='green' disabled={!bonus.active}>
+                    {get(currentLangData, `www.go-to-site`, '')}
+                  </Button>
+                </Container>
+              </Segment>
+            </Grid.Column>
+          ))}
+        </Grid>
       </Container>
     );
 };
